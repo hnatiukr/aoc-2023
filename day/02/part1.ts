efa2e300f4eeef32661342d1c21fbe2f47bea723
Roman Hnatiuk <roman.hnatiuk@openvpn.net>
2023-12-03 00:36:08 +0100

    day(02): solve part1

--- day/02/part1.ts
+++ day/02/part1.ts
@@ -1,3 +1,54 @@
-function part1(input: string[]) {}
+type Cubes = {
+    red: number;
+    green: number;
+    blue: number;
+};
+
+type Round = Cubes & { id: number };
+
+const BAG_LIMITS: Cubes = { red: 12, green: 13, blue: 14 } as const;
+
+//
+
+function matchGameId(line: string) {
+    const regex = /Game (\d+):/;
+    const maybeGameId = line.match(regex);
+    return maybeGameId ? Number.parseInt(maybeGameId[1], 10) : 0;
+}
+
+function makeRounds(line: string) {
+    const gameId = matchGameId(line);
+    const regex = /(\d+) (blue|green|red)/g;
+
+    return line.split(';').reduce((acc, set) => {
+        let match: RegExpExecArray | null = null;
+        const cubes: Round = { red: 0, green: 0, blue: 0, id: gameId };
+
+        while ((match = regex.exec(set)) !== null) {
+            const [, number, color] = match;
+
+            cubes[color] += Number.parseInt(number, 10);
+        }
+
+        return [...acc, cubes];
+    }, [] as Round[]);
+}
+
+function isGamePossible(rounds: Round[]) {
+    return rounds.every(
+        ({ red, green, blue }) =>
+            red <= BAG_LIMITS.red && green <= BAG_LIMITS.green && blue <= BAG_LIMITS.blue,
+    );
+}
+
+function calculateSum(sum: number, game: Round[]) {
+    return (sum += game[0].id);
+}
+
+//
+
+function part1(input: string[]) {
+    return input.map(makeRounds).filter(isGamePossible).reduce(calculateSum, 0);
+}
 
 export default part1;
